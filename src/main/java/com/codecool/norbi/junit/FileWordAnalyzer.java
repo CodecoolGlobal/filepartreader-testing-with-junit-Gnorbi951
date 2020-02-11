9711997e641914c1a17df15eb772391f6290906f
Gnorbi951 <gnorbi951@gmail.com>
2020-02-11 10:29:04 +0100

    Remove ',' from words

--- src/main/java/com/codecool/norbi/junit/FileWordAnalyzer.java
+++ src/main/java/com/codecool/norbi/junit/FileWordAnalyzer.java
@@ -18,17 +18,31 @@ public class FileWordAnalyzer {
         List<String> unorderedList = new ArrayList<>();
         // This is for filling the list with the words
         for (String word : input) {
-            if (word.contains("\n")) {
-                // TODO: If word contains , delete it
-                String[] splitWord = word.split("\n");
-                unorderedList.add(splitWord[0]);
-            } else {
-                unorderedList.add(word);
-            }
+            processWord(unorderedList, word);
         }
 
-        //Collections.sort(unorderedList);
+        Collections.sort(unorderedList);
         return unorderedList; // That is ordered
+    }
+
+    private void processWord(List<String> unorderedList, String word) {
+        if (word.contains("\n")) {
+            String[] splitWord = word.split("\n");
+            if (splitWord[0].contains(",")) {
+                String rightString = "";
+                for (int i = 0; i < splitWord[0].length(); i++) {
+                    char currentChar = splitWord[0].charAt(i);
+                    if (currentChar != ',') {
+                        rightString += currentChar;
+                    }
+                }
+                unorderedList.add(rightString);
+            } else {
+                unorderedList.add(splitWord[0]);
+            }
+        } else {
+            unorderedList.add(word);
+        }
     }
 
 }
