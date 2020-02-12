be2b473bca7a5af3f557a2c171c15f80619263c9
Gnorbi951 <gnorbi951@gmail.com>
2020-02-12 11:40:21 +0100

    Cover every file with tests

--- src/main/java/com/codecool/norbi/junit/Main.java
+++ src/main/java/com/codecool/norbi/junit/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         FilePartReader reader = new FilePartReader();
-        reader.setup("C:\\Codecool\\OOP\\5th_SI\\filepartreader-testing-with-junit-Gnorbi951\\test.txt", 1,1);
+        reader.setup("C:\\Codecool\\OOP\\5th_SI\\filepartreader-testing-with-junit-Gnorbi951\\test.txt", 1,10);
         System.out.println(reader.readLines());
         FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
         List<String> orderedAlphabetically =  analyzer.getWordsOrderedAlphabetically();
--- tests/com/codecool/norbi/junit/FileWordAnalyzerTest.java
+++ tests/com/codecool/norbi/junit/FileWordAnalyzerTest.java
@@ -1,13 +1,48 @@
 package com.codecool.norbi.junit;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
     FilePartReader reader = new FilePartReader();
-    FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
 
     @org.junit.jupiter.api.Test
     public void constructorGetsArgumentFromFilePartReader() {
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
         assertNotNull(analyzer);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void wordsGetOrderedAlphabetically() throws IOException {
+        reader.setup("C:\\Codecool\\OOP\\5th_SI\\filepartreader-testing-with-junit-Gnorbi951\\test.txt", 1,3);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
+        List<String> result = analyzer.getWordsOrderedAlphabetically();
+        List<String> expected = new ArrayList<>(Arrays.asList("According", "a", "abba", "all",
+                                                "aviation", "bee", "is", "known", "laws", "no", "to", "way"));
+        assertEquals(result, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void returnWordsContainingSubstring() throws IOException {
+        reader.setup("C:\\Codecool\\OOP\\5th_SI\\filepartreader-testing-with-junit-Gnorbi951\\test.txt", 1,10);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
+        List<String> result = analyzer.getWordsContainingSubstring("win");
+        List<String> expected = new ArrayList<>(Collections.singletonList("wings"));
+
+        assertEquals(result, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void findsPalindrome() throws IOException {
+        reader.setup("C:\\Codecool\\OOP\\5th_SI\\filepartreader-testing-with-junit-Gnorbi951\\test.txt", 1,3);
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(reader);
+        List<String> result = analyzer.getStringsWhichPalindromes();
+        List<String> expected = new ArrayList<>(Collections.singletonList("abba"));
+        assertEquals(result, expected);
     }
 }
